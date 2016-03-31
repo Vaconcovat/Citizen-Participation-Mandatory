7a8b76f58463b433f4a7694d0a31e1ed39a38913
Vaconcovat <vaconcovat@gmail.com>
2016-03-31 14:49:39 +1000

    Project organisation + Menus work
    Sorted asset types into folders, and started work on creating multiple
    scenes. there's a script with some static variables that store the rep
    values, money and so forth

---
+++ Assets/SceneChanger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneChanger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Arena(){
+		SceneManager.LoadScene("Test");
+	}
+
+	public void PreArena(){
+		SceneManager.LoadScene("preArena");
+	}
+
+	public void PostArena(){
+		SceneManager.LoadScene("postArena");
+	}
+}
---
+++ Assets/Scripts/StaticGameStats.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StaticGameStats : MonoBehaviour {
+	
+	static public float govRep;
+	static public float sponsorRep;
+	static public float rebelRep;
+	static public int money;
+	static public int arenasPlayed;
+
+	void Start(){
+
+	}
+
+}
