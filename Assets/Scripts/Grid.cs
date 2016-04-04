fc847aee281cdbad66ee835cd8bece0802b24bfa
the42ndfish <the42ndfish@gmail.com>
2016-04-04 19:52:22 +1000

    Addition of Nodes System
    Added node based terrain definition (Boxes appear where inpassable
    terrain is)

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour {
+
+    public Transform player;
+    public LayerMask unwalkableMask;
+    public Vector2 gridWorldSize;
+    public float nodeRadius;
+    Node[,] grid;
+
+    float nodeDiameter;
+    int gridSizeX, gridSizeY;
+
+    void Start()
+    {
+        nodeDiameter = nodeRadius * 2;
+        gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+        gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+        CreateGrid();
+    }
+
+    void CreateGrid()
+    {
+        grid = new Node[gridSizeX, gridSizeY];
+        Vector2 worldBottomLeft = (Vector2)transform.position - Vector2.right * gridWorldSize.x / 2 - Vector2.up * gridWorldSize.y / 2;
+        for (int x = 0; x < gridSizeX; x++) {
+            for (int y = 0; y < gridSizeY; y++) {
+                Vector2 worldPoint = worldBottomLeft + Vector2.right * (x * nodeDiameter + nodeRadius) + Vector2.up * (y * nodeDiameter + nodeRadius);
+                bool walkable = !(Physics2D.OverlapCircle(worldPoint, nodeRadius,unwalkableMask));
+                grid[x, y] = new Node(walkable, worldPoint);
+            }
+        }
+    }
+
+    public Node NodeFromWorldPoint(Vector2 worldPositon)
+    {
+        float percentX = (worldPositon.x + gridWorldSize.x / 2) / gridWorldSize.x;
+        float percentY = (worldPositon.y + gridWorldSize.y / 2) / gridWorldSize.y;
+        percentX = Mathf.Clamp01(percentX);
+        percentY = Mathf.Clamp01(percentY);
+
+        int x = Mathf.RoundToInt((gridSizeX - 1) * percentX);
+        int y = Mathf.RoundToInt((gridSizeY - 1) * percentY);
+        return grid[x, y];
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector2(gridWorldSize.x, gridWorldSize.y));
+
+        if (grid != null)
+        {
+            Node playerNode = NodeFromWorldPoint(player.position);
+            foreach ( Node n in grid) {
+                Gizmos.color = (n.walkable) ? Color.white : Color.red;
+                if (playerNode == n)
+                {
+                    Gizmos.color = Color.cyan;
+                }
+                Gizmos.DrawCube(n.worldPosition, Vector2.one * (nodeDiameter - .1f));
+            }
+        }
+    }
+}
