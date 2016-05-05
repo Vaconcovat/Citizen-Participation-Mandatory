032910fcc0658168f2c2f00be5fc7274264d4209
Vaconcovat <vaconcovat@gmail.com>
2016-05-05 15:33:27 +1000

    alternate arena, bloodsplatters, tron
    +Experimenting with bloom for a glowing tron feel
    +Alternate arena layout with a grid based floor (the previous one is
    still there)
    +Blood now fades away and slowly grows

--- Assets/Scripts/Global/SelfDestruct.cs
+++ Assets/Scripts/Global/SelfDestruct.cs
@@ -3,14 +3,36 @@ using System.Collections;
 
 public class SelfDestruct : MonoBehaviour {
 
+	public bool timed;
 	public float time;
+	public bool fade;
+	public float fadeRate;
+	public bool expand;
+	public float expandRate;
+
 	
 	// Update is called once per frame
 	void Update () {
-		time -= Time.deltaTime;
-		if (time <= 0){
-			Destroy(gameObject);
+		//reduce the time if enabled
+		if(timed){
+			time -= Time.deltaTime;
+			if (time <= 0){
+				Destroy(gameObject);
+			}
 		}
 
+		//fade if enabled
+		if (fade){
+			Color previous = GetComponent<SpriteRenderer>().color;
+			GetComponent<SpriteRenderer>().color = new Color(previous.r,previous.g,previous.b,previous.a - fadeRate);
+			if (previous.a <= 0.01){
+				Destroy(gameObject);
+			}
+		}
+
+		//expand if enabled
+		if (expand){
+			transform.localScale = new Vector3(transform.localScale.x + expandRate, transform.localScale.y + expandRate, 1);
+		}
 	}
 }
