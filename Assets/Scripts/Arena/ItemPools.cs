5678e8fc2de36c9627e1faa0371a1261d6785822
Vaconcovat <vaconcovat@gmail.com>
2016-05-10 14:43:10 +1000

    Item spawners
    +All weapons/items need to be made as prefabs, then assigned to an item
    pool on the global scripts object.
    +There's 3 example item spawners in the game now: "Example"(all the
    guns), "Sponsor" (Just the wacky gun) and "Items" (The hp kit and drink)

---
+++ Assets/Scripts/Arena/ItemPools.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemPools : MonoBehaviour {
+	public GameObject[] Example_Pool;
+	public GameObject[] Sponsor_Pool;
+	public GameObject[] Item_Pool;
+	public Pool example;
+	public Pool sponsor;
+	public Pool item;
+
+	// Use this for initialization
+	void Awake () {
+		example = new Pool(Example_Pool);
+		sponsor = new Pool(Sponsor_Pool);
+		item = new Pool(Item_Pool);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public class Pool{
+		public GameObject[] items;
+
+		//constructor
+		public Pool(GameObject[] items){
+			this.items = items;
+		}
+	}
+}
---
+++ Assets/Scripts/ItemSpawner.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemSpawner : MonoBehaviour {
+	public enum poolselection{Example, Sponsor, Item};
+	public poolselection selection;
+	public ItemPools.Pool pool;
+
+	// Use this for initialization
+	void Start () {
+		switch(selection){
+			case poolselection.Example:
+				pool = FindObjectOfType<ItemPools>().example;
+				break;
+			case poolselection.Sponsor:
+				pool = FindObjectOfType<ItemPools>().sponsor;
+				break;
+			case poolselection.Item:
+				pool = FindObjectOfType<ItemPools>().item;
+				break;
+		}
+		GameObject spawned = (GameObject)Instantiate(pool.items[Random.Range(0,pool.items.Length)],this.transform.position,Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
