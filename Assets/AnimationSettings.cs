05e118abfe90886f1c1fd7f7680292be5f537b01
Steven Farnfield <steven.farnfield@hotmail.com>
2016-05-26 11:21:29 +1000

    Animation Trigger
    Animation now only runs on W A S D

---
+++ Assets/AnimationSettings.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationSettings : MonoBehaviour {
+
+	public KeyCode Left;
+	public KeyCode Right;
+	public KeyCode Up;
+	public KeyCode Down;
+	public string MyTrigger;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (Left)) {
+			GetComponent<Animator> ().SetTrigger (MyTrigger);
+		}
+
+		if (Input.GetKey (Right)) {
+			GetComponent<Animator> ().SetTrigger (MyTrigger);
+		}
+
+		if (Input.GetKey (Up)) {
+			GetComponent<Animator> ().SetTrigger (MyTrigger);
+		}
+
+		if (Input.GetKey (Down)) {
+			GetComponent<Animator> ().SetTrigger (MyTrigger);
+		}
+	
+	}
+}
