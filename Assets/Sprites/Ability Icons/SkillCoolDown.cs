07f5266cf46eaae92a8e26de5fdd4afb3921de1a
Steven Farnfield <steven.farnfield@hotmail.com>
2016-06-11 14:41:26 +1000

    ABILITIES
    - Changed the Canvas so all the elements now scale with Resolution
    (ISSUE: Some UI Objects now need to be resized for native 1920x1080)
    - Added an Ability Bar with 5 abilities tied to Numbers 1 - 5 on the
    keyboard, pressing these keys will trigger the abilities and reset the
    cooldown (ISSUE: The Ability Bar is currently rendering below other in
    game objects unlike the other objects in the canvas which render Above
    other in game objects)
    - The Abilites Currently do not have an ingame effect

---
+++ Assets/Sprites/Ability Icons/SkillCoolDown.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class SkillCoolDown : MonoBehaviour {
+
+	public List<Skill> skills;
+	public KeyCode Ability1;
+	public KeyCode Ability2;
+	public KeyCode Ability3;
+	public KeyCode Ability4;
+	public KeyCode Ability5;
+
+	void FixedUpdate()
+	{
+		if (Input.GetKey (Ability1)) 
+		{
+			if (skills [0].currentCooldown >= skills [0].cooldown) 
+			{
+				//Whatever Skill 1 Does
+				skills [0].currentCooldown = 0;
+			}
+		}
+
+		else if (Input.GetKey (Ability2)) 
+		{
+			if (skills [1].currentCooldown >= skills [1].cooldown) 
+			{
+				//Whatever Skill 1 Does
+				skills [1].currentCooldown = 0;
+			}
+		}
+
+		else if (Input.GetKey (Ability3)) 
+		{
+			if (skills [2].currentCooldown >= skills [2].cooldown) 
+			{
+				//Whatever Skill 1 Does
+				skills [2].currentCooldown = 0;
+			}
+		}
+
+		else if (Input.GetKey (Ability4)) 
+		{
+			if (skills [3].currentCooldown >= skills [3].cooldown) 
+			{
+				//Whatever Skill 1 Does
+				skills [3].currentCooldown = 0;
+			}
+		}
+
+		else if (Input.GetKey (Ability5)) 
+		{
+			if (skills [4].currentCooldown >= skills [4].cooldown) 
+			{
+				//Whatever Skill 1 Does
+				skills [4].currentCooldown = 0;
+			}
+		}
+	}
+
+	void Update()
+	{
+		foreach (Skill s in skills) 
+		{
+			if (s.currentCooldown < s.cooldown) 
+			{
+				s.currentCooldown += Time.deltaTime;
+				s.skillIcon.fillAmount = s.currentCooldown / s.cooldown;
+			}
+
+		}
+	}
+}
+
+[System.Serializable]
+public class Skill
+{
+	public float cooldown;
+	public Image skillIcon;
+	[HideInInspector]
+	public float currentCooldown;
+}
+
