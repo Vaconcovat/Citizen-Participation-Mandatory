4abef6d4730d9c6fe62e179bb1aba8b5c9a42b6a
Steven Farnfield <steven.farnfield@hotmail.com>
2016-07-04 13:01:42 +1000

    Basic Animation System
    - Added Script Titled AnimationHandler in Arena Scripts
    - Script allows an object to be given any number of animations and
    button activators and will test for all of them simultaneously

---
+++ Assets/Scripts/Arena/AnimationHandler.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AnimationHandler : MonoBehaviour {
+
+	public List<Animation> animations = new List<Animation>();
+	int sizeofList;
+
+	// Use this for initialization
+	void Start () {
+		sizeofList = animations.Count;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i <= sizeofList; i++) 
+		{
+			if (Input.GetKey (animations [i].Button)) {
+				Debug.Log (animations [i].Button); //Testing Purposes right now. but here is where you would tell unity to play the animation
+			}
+		}
+	}
+}
+
+[System.Serializable]
+public class Animation
+{
+	[Tooltip("The Key that will need to be pressed to run the animation")]
+	public KeyCode Button; 
+	[Tooltip("The Animation that will be played")]
+	public AnimationClip AnimationToBeRun; 
+}
