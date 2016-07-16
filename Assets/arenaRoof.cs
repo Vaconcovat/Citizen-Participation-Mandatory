aef86ea057d1fb0d00c9d9779e601cdf2e12f43e
Vaconcovat <vaconcovat@gmail.com>
2016-07-16 13:18:08 +1000

    Outer arena areas, roof toggles
    -there are now external bays for units to enter / exit the arena
    -entering / exiting the arena now can toggle objects - this can be
    useful to simulate a crowd outside the arena / not show the arena when
    not inside it

---
+++ Assets/arenaRoof.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class arenaRoof : MonoBehaviour {
+
+public GameObject roof, outerRoof;
+public bool inArena = true;
+
+	// Use this for initialization
+	void Start () {
+		if(!inArena){
+					roof.SetActive(true);
+					outerRoof.SetActive(false);
+				}else{
+					roof.SetActive(false);
+					outerRoof.SetActive(true);
+				}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnTriggerExit(Collider col){
+		if (col.gameObject.tag == "Contestant"){
+			if(col.gameObject.GetComponent<Contestant>().isPlayer){
+				if(inArena){
+					roof.SetActive(true);
+					outerRoof.SetActive(false);
+				}else{
+					roof.SetActive(false);
+					outerRoof.SetActive(true);
+				}
+				inArena = !inArena;
+
+
+			}
+		}
+	}
+}
