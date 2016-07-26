74a893dfd9b583190172fca78de61c11760b4051
Froodleme <lettuce.cruncher@gmail.com>
2016-07-26 18:57:32 +1000

    Win screen changes
    Added in win screen context for player, in same vein as lose screen
    flavour wise as it seems on point for the Government to kill you either
    way as a measure of keeping things efficient.

--- Assets/WinInterfaceManager.cs
+++ Assets/WinInterfaceManager.cs
@@ -1,15 +1,36 @@
 ﻿using UnityEngine;
+using UnityStandardAssets.ImageEffects;
 using System.Collections;
 
 public class WinInterfaceManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public AutoType at;
+    NoiseAndScratches ns;
+    bool fin = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        ns = FindObjectOfType<NoiseAndScratches>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fin)
+        {
+            ns.grainIntensityMin += Time.deltaTime;
+            ns.grainIntensityMax += Time.deltaTime;
+        }
+    }
+
+    public void Lose()
+    {
+        at.StartType();
+    }
+
+    public void Finished()
+    {
+        fin = true;
+    }
 }
