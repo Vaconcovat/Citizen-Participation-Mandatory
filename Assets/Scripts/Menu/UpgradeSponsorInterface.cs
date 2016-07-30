03f2b817642cb8f78f0119b2756b9028032756fc
Steven Farnfield <steven.farnfield@hotmail.com>
2016-07-30 15:58:10 +1000

    Bug Fix Commit
    - Updated Player Traits
    - Fixed Embezzle Text not tracking
    - Fixed Funding Text not tracking

---
+++ Assets/Scripts/Menu/UpgradeSponsorInterface.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UpgradeSponsorInterface : MonoBehaviour {
+
+	public Text moneyText, embezText;
+
+	// Update is called once per frame
+	void Update () {
+		moneyText.text = "Funding:" + StaticGameStats.avaliableMoney.ToString();
+		embezText.text = StaticGameStats.embezzledMoney.ToString();
+	}
+
+	public void AddMoney(){
+		if(StaticGameStats.avaliableMoney >= 1){
+			StaticGameStats.avaliableMoney--;
+			StaticGameStats.embezzledMoney++;
+		}
+	}
+
+	public void AddMoney5(){
+		if(StaticGameStats.avaliableMoney >= 5){
+			for (int i = 0; i <= 4; i++) {
+				StaticGameStats.avaliableMoney--;
+				StaticGameStats.embezzledMoney++;
+			}
+		}
+	}
+
+	public void AddMoney10(){
+		if(StaticGameStats.avaliableMoney >= 10){
+			for (int i = 0; i <= 9; i++) {
+				StaticGameStats.avaliableMoney--;
+				StaticGameStats.embezzledMoney++;
+			}	
+		}
+	}
+}
