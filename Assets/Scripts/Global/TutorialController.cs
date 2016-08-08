3ff0933005851b0ee6d69a2a39f0c7d8a3e4fac0
the42ndfish <the42ndfish@gmail.com>
2016-08-08 16:42:13 +1000

    Tutorial Progression System Basic
    Implemented the ability to progress through the level and give visual
    cues such as lights swapping to yellow and removing walls to allow the
    player to move through the scene.

---
+++ Assets/Scripts/Global/TutorialController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialController : MonoBehaviour {
+
+	public Light light1;
+	public Light light2;
+	public Light light3;
+	public Light light4;
+
+
+	// Use this for initialization
+	void Start () {
+		light1 = GetComponent<Light>();
+		light2 = GetComponent<Light>();
+		light3 = GetComponent<Light>();
+		light4 = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		light1.color -= Color.white / 2.0F * Time.deltaTime;
+		light2.color -= Color.white / 2.0F * Time.deltaTime;
+		light3.color -= Color.white / 2.0F * Time.deltaTime;
+		light4.color -= Color.white / 2.0F * Time.deltaTime;
+	}
+}
