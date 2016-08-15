237f9dc877e1db7e97e018f73c4fd7d96a22b672
Froodleme <lettuce.cruncher@gmail.com>
2016-08-15 14:24:12 +1000

    Third Sponsor Weapon
    The first of two sponsor weapons for the third sponsor is now in place,
    the sniper rifle.
    
    This weapon will deal high damage but is countered with a high fire rate
    and low ammo. It can also pierce enemies, up to one additional from its
    initial target.

--- Assets/Scripts/Arena/ItemPools.cs
+++ Assets/Scripts/Arena/ItemPools.cs
@@ -5,19 +5,22 @@ public class ItemPools : MonoBehaviour {
 	public GameObject[] Example_Pool;
 	public GameObject[] Sponsor0_Pool;
 	public GameObject[] Sponsor1_Pool;
-	public GameObject[] Item_Pool;
+    public GameObject[] Sponsor2_Pool;
+    public GameObject[] Item_Pool;
 	public GameObject[] NoHealthKitItem_Pool;
 	public Pool example;
 	public Pool sponsor0;
 	public Pool sponsor1;
-	public Pool item;
+    public Pool sponsor2;
+    public Pool item;
 
 	// Use this for initialization
 	void Awake () {
 		example = new Pool(Example_Pool);
 		sponsor0 = new Pool(Sponsor0_Pool);
 		sponsor1 = new Pool(Sponsor1_Pool);
-		if (StaticGameStats.TierTwoUpgrades [2]) {
+        sponsor2 = new Pool(Sponsor2_Pool);
+        if (StaticGameStats.TierTwoUpgrades [2]) {
 			item = new Pool (NoHealthKitItem_Pool);
 		} else {
 			item = new Pool(Item_Pool);
