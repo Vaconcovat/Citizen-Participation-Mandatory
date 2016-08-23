217433b9de4b434e7e0c8c4f33dbb6dfb7a3fad5
Thomas <n9161201@qut.edu.au>
2016-08-23 16:16:05 +1000

    Tutorial is a bit broke
    Added script to display changes to influence to a text UI asset and
    changed some stuff in the tutorial scene

---
+++ Assets/Scripts/ChangesToInfluence.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class ChangesToInfluence : MonoBehaviour {
+
+	public TextAsset influenceFile;
+	private List<string> influenceList;
+	public Text textObj;
+
+	void Awake  () {
+		string influenceStr = influenceFile.text;
+
+		influenceList = new List<string>();
+		influenceList.AddRange(influenceStr.Split("\n"[0]));
+	}
+
+	// Use this for initialization
+	void Start () {
+		string[] infArray = influenceList.ToArray();
+		for (int i = 0; i < influenceList.Count; i++) {
+			foreach (char letter in infArray[i].ToCharArray()) {
+				textObj.text += letter;
+			}
+			textObj.text += "\n";
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
