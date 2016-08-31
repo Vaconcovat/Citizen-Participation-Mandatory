bd4374b3d54a0b0884301fcc9d93afa8eb4ca8ef
the42ndfish <the42ndfish@gmail.com>
2016-08-31 21:10:28 +1000

    Fixed Bug for progressing to arena
    Progressing to the arena could occur if any object entered the end area,
    including a thrown weapon.

--- Assets/LoadArena.cs
+++ Assets/LoadArena.cs
@@ -6,7 +6,9 @@ public class LoadArena : MonoBehaviour {
 
 	void OnTriggerEnter (Collider other)
 	{
-		RoundManager.roundNumber = 1;
-		SceneManager.LoadScene(1);
+		if (other.gameObject.tag == "Contestant") {
+			RoundManager.roundNumber = 1;
+			SceneManager.LoadScene(1);
+		}
 	}
 }
