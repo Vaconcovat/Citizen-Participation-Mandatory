649af36839207a173f07f15357691d2377b485c1
Modification102 <lost_boy2201@hotmail.com>
2016-09-07 13:48:14 +1000

    Questionnaire Updated
    Removed Incorrect Fields
    Removed Government Secrets Fields
    Reduced Rebel Fields to a single optional text input field
    submit button is clickable when all the information is entered
    Added Headings to the sections

--- Assets/QuestionaireManager.cs
+++ Assets/QuestionaireManager.cs
@@ -14,14 +14,13 @@ public class QuestionaireManager : MonoBehaviour {
 	public Text ConstructionIncorrect;
 	public InputField RebelFName;
 	public InputField RebelLName;
-	public InputField GovPlans;
 	public InputField Username;
 	public Toggle MindControl;
 	public Toggle Deaths;
 	public Toggle Performance;
 	public Toggle Funding;
-	public Toggle Rebels;
 	public Toggle GovernmentPlans;
+	public Toggle GovernmentPlans2;
 	public Image Redacted1;
 	public Image Redacted2;
 	public Image Redacted3;
@@ -38,47 +37,12 @@ public class QuestionaireManager : MonoBehaviour {
 	public Text RedactedText5;
 	public Text RedactedText6;
 	public Text RedactedText7;
-	
-	// Update is called once per frame
-	void Update () {
-		if ((Username.text != "") && (MindControl.isOn == true) && (Deaths.isOn == true) && (Performance.isOn == true) && (Funding.isOn == true) && (RebelFName.text != "") && (RebelLName.text != "") && (Rebels.isOn == false) && (GovPlans.text == "") && (GovernmentPlans.isOn == true)) {
+
+	void Update(){
+		if ((Username.text != "") && (MindControl.isOn) && (Deaths.isOn) && (Performance.isOn) && (Funding.isOn)) {
 			Submit.interactable = true;
 		} else {
 			Submit.interactable = false;
-		}
-	}
-
-	public void RebelIncorrect(){
-		if (Rebels.isOn) {
-			RebelIncorrect2.enabled = true;
-		} else {
-			RebelIncorrect2.enabled = false;
-		}
-
-		if (RebelFName.text == "") {
-			RebelIncorrect1.enabled = true;
-		}
-
-		if (RebelLName.text == "") {
-			RebelIncorrect1.enabled = true;
-		}
-
-		if ((RebelFName.text != "") && (RebelLName.text != "")) {
-			RebelIncorrect1.enabled = false;
-		}
-	}
-
-	public void GovernmentIncorrect(){
-		if (GovPlans.text == "") {
-			GovSecretIncorrect1.enabled = false;
-		} else {
-			GovSecretIncorrect1.enabled = true;
-		}
-
-		if (GovernmentPlans.isOn) {
-			GovSecretIncorrect2.enabled = false;
-		} else {
-			GovSecretIncorrect2.enabled = true;
 		}
 	}
 
@@ -101,23 +65,23 @@ public class QuestionaireManager : MonoBehaviour {
 			RedactedText1.enabled = false;
 			Redacted2.enabled = false;
 			RedactedText2.enabled = false;
-			MindControlIncorrect.enabled = false;
+			//MindControlIncorrect.enabled = false;
 		} else {
 			Redacted1.enabled = true;
 			RedactedText1.enabled = true;
 			Redacted2.enabled = true;
 			RedactedText2.enabled = true;
-			MindControlIncorrect.enabled = true;
+			//MindControlIncorrect.enabled = true;
 		}
 
 		if (Deaths.isOn) {
 			Redacted3.enabled = false;
 			RedactedText3.enabled = false;
-			DeathsIncorrect.enabled = false;
+			//DeathsIncorrect.enabled = false;
 		} else {
 			Redacted3.enabled = true;
 			RedactedText3.enabled = true;
-			DeathsIncorrect.enabled = true;
+			//DeathsIncorrect.enabled = true;
 		}
 
 		if (Performance.isOn) {
@@ -125,13 +89,13 @@ public class QuestionaireManager : MonoBehaviour {
 			RedactedText4.enabled = false;
 			Redacted5.enabled = false;
 			RedactedText5.enabled = false;
-			JudgedIncorrect.enabled = false;
+			//JudgedIncorrect.enabled = false;
 		} else {
 			Redacted4.enabled = true;
 			RedactedText4.enabled = true;
 			Redacted5.enabled = true;
 			RedactedText5.enabled = true;
-			JudgedIncorrect.enabled = true;
+			//JudgedIncorrect.enabled = true;
 		}
 
 		if (Funding.isOn) {
@@ -139,13 +103,17 @@ public class QuestionaireManager : MonoBehaviour {
 			RedactedText6.enabled = false;
 			Redacted7.enabled = false;
 			RedactedText7.enabled = false;
-			ConstructionIncorrect.enabled = false;
+			//ConstructionIncorrect.enabled = false;
 		} else {
 			Redacted6.enabled = true;
 			RedactedText6.enabled = true;
 			Redacted7.enabled = true;
 			RedactedText7.enabled = true;
-			ConstructionIncorrect.enabled = true;
+			//ConstructionIncorrect.enabled = true;
 		}
+
+
+
 	}
+		
 }
