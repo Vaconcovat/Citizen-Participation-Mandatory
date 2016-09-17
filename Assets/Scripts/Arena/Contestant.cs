207a4424f0619dad6b59203dc4b472caea645206
Steven Farnfield <steven.farnfield@hotmail.com>
2016-09-17 17:15:38 +1000

    Merciful Men Upgrade Finished

--- Assets/Scripts/Arena/Contestant.cs
+++ Assets/Scripts/Arena/Contestant.cs
@@ -132,33 +132,52 @@ public class Contestant : MonoBehaviour {
 
         //----------------------------------------------------
         //GENERATE TRAITS
+		if (StaticGameStats.TierTwoUpgrades [2]) {
+			//20% chance to be sick, 20% chance to be strong
+			float random = Random.value;
+			if (random < 0.2f) {
+				traits.Add (Trait.Sick);
+			} else if (random > 0.8f) {
+				traits.Add (Trait.Strong);
+			}
 
-        //20% chance to be sick, 20% chance to be strong
-        float random = Random.value;
-        if (random < 0.2f){
-        	traits.Add(Trait.Sick);
-        }
-        else if (random > 0.8f){
-        	traits.Add(Trait.Strong);
-        }
+			//20% chance to be scared, 20% chance to be fearless
+			random = Random.value;
+			if (random < 0.2f) {
+				traits.Add (Trait.Scared);
+			} else if (random > 0.8f) {
+				traits.Add (Trait.Fearless);
+			}
 
-        //20% chance to be scared, 20% chance to be fearless
-		random = Random.value;
-		if (random < 0.2f){
-        	traits.Add(Trait.Scared);
-        }
-        else if (random > 0.8f){
-        	traits.Add(Trait.Fearless);
-        }
+			//Always Applies the Merciful Trait
+			traits.Add (Trait.Merciful);
+		} else {
+			//20% chance to be sick, 20% chance to be strong
+			float random = Random.value;
+			if (random < 0.2f) {
+				traits.Add (Trait.Sick);
+			} else if (random > 0.8f) {
+				traits.Add (Trait.Strong);
+			}
 
-		//20% chance to be merciful, 20% chance to be relentless
-		random = Random.value;
-		if (random < 0.2f){
-        	traits.Add(Trait.Merciful);
-        }
-        else if (random > 0.8f){
-        	traits.Add(Trait.Relentless);
-        }
+			//20% chance to be scared, 20% chance to be fearless
+			random = Random.value;
+			if (random < 0.2f) {
+				traits.Add (Trait.Scared);
+			} else if (random > 0.8f) {
+				traits.Add (Trait.Fearless);
+			}
+
+			//20% chance to be merciful, 20% chance to be relentless
+			random = Random.value;
+			if (random < 0.2f) {
+				traits.Add (Trait.Merciful);
+			} else if (random > 0.8f) {
+				traits.Add (Trait.Relentless);
+			}
+		}
+
+        
 
         //
         //------------------------------------------------------
