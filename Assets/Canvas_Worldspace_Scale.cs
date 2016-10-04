121cb0b559094cb5a22b471848c833f1c8a414e7
Vaconcovat <vaconcovat@gmail.com>
2016-10-04 17:07:13 +1000

    Aspect ratio commit! + settings fix
    Fixed settings not actually being persistent (you can't use different
    playerpref keys in the same frame!)
    
    Added a canvas worldspace scaler
    
    Modified all ui canvases to work with 16:9 and 16:10 resolutions

---
+++ Assets/Canvas_Worldspace_Scale.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent (typeof (UnityEngine.UI.AspectRatioFitter))]
+public class Canvas_Worldspace_Scale : MonoBehaviour {
+
+	void OnEnable(){
+		GetComponent<AspectRatioFitter>().aspectRatio = (Screen.width * 1.0f) / (Screen.height * 1.0f);
+	}
+}
--- Assets/SettingsManager.cs
+++ Assets/SettingsManager.cs
@@ -10,9 +10,11 @@ public class SettingsManager : MonoBehaviour {
 	int analytics;
 	
 	// Use this for initialization
-	void Awake () {
+	void OnEnable () {
 		if(PlayerPrefs.HasKey("SFXVolume")){
 			sfxVolume = PlayerPrefs.GetFloat("SFXVolume");
+			Debug.Log("Prefs has SFXVOLUME: " + sfxVolume);
+			sfxSlider.value = sfxVolume;
 		}
 		else{
 			sfxVolume = 1;
@@ -21,6 +23,8 @@ public class SettingsManager : MonoBehaviour {
 
 		if(PlayerPrefs.HasKey("MusicVolume")){
 			musicVolume = PlayerPrefs.GetFloat("MusicVolume");
+			Debug.Log("Prefs has MusicVolume: " + musicVolume);
+			musicSlider.value = musicVolume;
 		}
 		else{
 			musicVolume = 1;
@@ -29,27 +33,34 @@ public class SettingsManager : MonoBehaviour {
 
 		if(PlayerPrefs.HasKey("Analytics")){
 			analytics = PlayerPrefs.GetInt("Analytics");
+			Debug.Log("Prefs has Analytics: " + analytics);
+			analyticsSlider.value = analytics;
 		}
 		else{
 			analytics = 1;
 			PlayerPrefs.SetInt("Analytics", 1);
 		}
 	}
+		
+	public void UpdatePrefs(int a){
+		switch(a){
+			case 0:
+				sfxVolume = sfxSlider.value;
+				PlayerPrefs.SetFloat("SFXVolume", sfxSlider.value);
+				sfxNum.text = string.Format("{0:P0}", sfxVolume);
+				break;
+			case 1:
+				musicVolume = musicSlider.value;
+				PlayerPrefs.SetFloat("MusicVolume", musicSlider.value);
+				musicNum.text = string.Format("{0:P0}", musicVolume);
+				break;
+			case 2:
+				PlayerPrefs.SetInt("Analytics", Mathf.RoundToInt(analyticsSlider.value));
+				analytics = Mathf.RoundToInt(analyticsSlider.value);
+				analyticsOn.color = (analytics == 1)?Color.white:new Color(1,1,1,0.5f);
+				analyticsOff.color = (analytics == 0)?Color.white:new Color(1,1,1,0.5f);
+				break;
+		}
 
-	void Start(){
-		UpdatePrefs();
-	}
-	
-	public void UpdatePrefs(){
-		PlayerPrefs.SetFloat("SFXVolume", sfxSlider.value);
-		sfxVolume = sfxSlider.value;
-		PlayerPrefs.SetFloat("MusicVolume", musicSlider.value);
-		musicVolume = musicSlider.value;
-		sfxNum.text = string.Format("{0:P0}", sfxVolume);
-		musicNum.text = string.Format("{0:P0}", musicVolume);
-		PlayerPrefs.SetInt("Analytics", Mathf.RoundToInt(analyticsSlider.value));
-		analytics = Mathf.RoundToInt(analyticsSlider.value);
-		analyticsOn.color = (analytics == 1)?Color.white:new Color(1,1,1,0.5f);
-		analyticsOff.color = (analytics == 0)?Color.white:new Color(1,1,1,0.5f);
 	}
 }
