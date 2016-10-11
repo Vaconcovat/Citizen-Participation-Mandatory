d8ad7c832fc3c694d462cd4fb4cffad5813b7760
Thomas <n9161201@qut.edu.au>
2016-10-11 20:55:20 +1000

    Camera particle effects
    Particle effect of sparks and smoke added to the cameras on death and
    finishes duration before destroying the effect

--- Assets/PowerBox.cs
+++ Assets/PowerBox.cs
@@ -5,6 +5,8 @@ public class PowerBox : MonoBehaviour {
 	public GameObject Camera;
 	public bool isAlive;
 	public int Health;
+	public GameObject flare;
+	bool hasDied = false;
 
 	void Start () {
 		Health = 25;
@@ -17,6 +19,10 @@ public class PowerBox : MonoBehaviour {
 		}
 		if (!isAlive) {
 			Kill(Camera);
+			if (!hasDied) {
+				CameraExplosion (Camera);
+				hasDied = true;
+			}
 		}
 
 	}
@@ -48,5 +54,15 @@ public class PowerBox : MonoBehaviour {
 	void Kill(GameObject g){
 		g.GetComponent<Arena_Camera>().enabled = false;
 		g.GetComponent<Rigidbody>().isKinematic = false;
+	}
+
+	void CameraExplosion (GameObject g)
+	{
+		//Debug.Log ("Health Kit Used");
+		FindObjectOfType<SoundManager>().PlayEffect(FindObjectOfType<SoundManager>().explosion, transform.position, 0.3f, true);
+		GameObject spawned = (GameObject)Instantiate (flare, g.transform.position, Quaternion.identity);
+		spawned.transform.Rotate (90,0,0);
+		spawned.transform.parent = this.transform;
+		Destroy(spawned, 4.0f);
 	}
 }
