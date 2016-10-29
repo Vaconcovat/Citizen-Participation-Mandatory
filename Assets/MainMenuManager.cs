1b4e75665c01793e15b1cd7eb17b1ec6dac73623
Vaconcovat <vaconcovat@gmail.com>
2016-10-29 21:29:08 +1000

    Logging alert
    Added an alert to the main screen when logging is enabled

---
+++ Assets/MainMenuManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenuManager : MonoBehaviour {
+	public Text loggingalert;
+		
+	// Update is called once per frame
+	void Update () {
+		if(PlayerPrefs.GetInt("Analytics") != 1){
+			loggingalert.enabled = false;
+		}
+		else{
+			loggingalert.enabled = true;
+		}
+	}
+}
